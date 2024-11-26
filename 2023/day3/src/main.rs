6b2d07e46516b0e28b8c243a22704cb8a98e38e9
BradenEverson <bradeneverson@gmail.com>
2024-11-26 08:50:04 -0600

    Create engine graph

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,1 +1,16 @@
+pub struct EngineGraph {
+    pub nodes: Vec<EngineNode>,
+}
+
+pub struct EngineNode {
+    pub ty: Symbol,
+    pub adjacent: Vec<Symbol>,
+}
+
+pub enum Symbol {
+    Symbol(char),
+    Empty,
+    Number(i32),
+}
+
 fn main() {}
