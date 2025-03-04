bc56285dd989f0b2e5ae461ea4ecb3f217eb5d1b
BradenEverson <bradeneverson@gmail.com>
2025-03-04 06:22:46 -0600

    Finish day2 part 1

--- 2018/day2/main.zig
+++ 2018/day2/main.zig
@@ -1,6 +1,63 @@
 const std = @import("std");
 const data = @embedFile("data/input");
 
+const StrResult = enum {
+    Two,
+    Three,
+    Both,
+    None,
+};
+
 pub fn main() void {
-    std.debug.print("Day 2\n", .{});
+    var vals = std.mem.split(u8, data, "\n");
+
+    var twos: usize = 0;
+    var threes: usize = 0;
+
+    while (vals.next()) |val| {
+        if (val.len == 0) continue;
+        const tag = findDupes(val);
+
+        switch (tag) {
+            StrResult.Two => twos += 1,
+            StrResult.Three => threes += 1,
+            StrResult.Both => {
+                twos += 1;
+                threes += 1;
+            },
+            StrResult.None => {},
+        }
+    }
+
+    const total = twos * threes;
+    std.debug.print("Checksum: {d}\n", .{total});
+}
+
+pub fn findDupes(str: []const u8) StrResult {
+    var charCounts: [26]u32 = std.mem.zeroes([26]u32);
+
+    for (str) |char| {
+        charCounts[char - 'a'] += 1;
+    }
+
+    var three: bool = false;
+    var two: bool = false;
+
+    for (charCounts) |count| {
+        if (count == 3) {
+            three = true;
+        } else if (count == 2) {
+            two = true;
+        }
+    }
+
+    if (three and !two) {
+        return StrResult.Three;
+    } else if (!three and two) {
+        return StrResult.Two;
+    } else if (three and two) {
+        return StrResult.Both;
+    } else {
+        return StrResult.None;
+    }
 }
