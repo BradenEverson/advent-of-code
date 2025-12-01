3a81a1928345afad997e591532f7dc037b3504bb
BradenEverson <bradeneverson@gmail.com>
2025-12-01 03:38:19 -0600

    Day 1 yay

---
+++ 2025/day1/part1.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const input = @embedFile("data/input");
+
+pub fn main() void {
+    var lines = std.mem.tokenizeAny(u8, input, "\n");
+    var curr: i32 = 50;
+    var zero_count: u32 = 0;
+
+    while (lines.next()) |line| {
+        const dir = line[0];
+        const amount = line[1..];
+
+        var mag: i32 = 1;
+        if (dir == 'L') {
+            mag = -1;
+        }
+
+        const val = std.fmt.parseInt(i32, amount, 10) catch @panic("Malformed Input :(");
+        curr += mag * val;
+        curr = @rem(curr, 100);
+
+        if (curr == 0) {
+            zero_count += 1;
+        }
+    }
+
+    std.debug.print("{d} zeros\n", .{zero_count});
+}
