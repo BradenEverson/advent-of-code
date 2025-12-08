b49bafff3d3eb239516ebf110084f71c817f92a2
BradenEverson <bradeneverson@gmail.com>
2025-12-08 06:27:07 -0600

    Graph based approach for the win

--- 2025/day8/part1.zig
+++ 2025/day8/part1.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
-const input = @embedFile("data/test");
+const input = @embedFile("data/input");
+
+const TEST_MAX: usize = 10;
+const MAX: usize = 1000;
 
 pub fn main() void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -14,6 +17,11 @@ pub fn main() void {
 
     var lines = std.mem.tokenizeAny(u8, input, "\n");
     var points: [vals]Point = undefined;
+    defer {
+        for (&points) |*point| {
+            point.deinit(alloc);
+        }
+    }
 
     var dists = std.ArrayList(Pair){};
     defer dists.deinit(alloc);
@@ -31,37 +39,67 @@ pub fn main() void {
     }
 
     for (0..points.len) |i| {
-        const a = points[i];
         for (i + 1..points.len) |j| {
-            const b = points[j];
-            dists.append(alloc, Pair.init(a, b)) catch @panic("Failed to push");
+            dists.append(alloc, Pair.init(&points[i], &points[j])) catch @panic("Failed to push");
         }
     }
 
     std.mem.sort(Pair, dists.items, {}, Pair.compare);
-    var circuit = Circuits{};
-    defer circuit.deinit(alloc);
 
     var connections: usize = 0;
 
     for (dists.items) |dist| {
-        const res = circuit.connect(alloc, dist) catch @panic("Failed to push circuit");
-        if (res) {
+        if (!dist.a.pointsTo(dist.b)) {
+            dist.a.connections.append(alloc, dist.b) catch @panic("Failed to push");
+            dist.b.connections.append(alloc, dist.a) catch @panic("Failed to push");
+
             connections += 1;
-            if (connections == 10) {
-                break;
-            }
+            if (connections == MAX) break;
         }
     }
 
-    std.mem.sort(Circuit, circuit.connections.items, {}, Circuit.compare);
-    var total: u64 = circuit.connections.items[0].points.items.len;
+    var seen = std.ArrayList(*Point){};
+    defer seen.deinit(alloc);
 
-    for (circuit.connections.items[1..3]) |c| {
-        total *= c.points.items.len;
+    var sizes = std.ArrayList(usize){};
+    defer sizes.deinit(alloc);
+
+    for (&points) |*point| {
+        if (std.mem.indexOf(*Point, seen.items, &[_]*Point{point}) != null) {
+            continue;
+        }
+
+        var size: u64 = 0;
+        sizeOfCircuit(point, &size, &seen, alloc) catch @panic("Graph traveler is messed up");
+        sizes.append(alloc, size) catch @panic("Failed to add size");
     }
 
-    std.debug.print("Total: {}\n", .{total});
+    std.mem.sort(u64, sizes.items, {}, compareU64);
+
+    var total = sizes.items[0];
+
+    for (sizes.items[1..3]) |size| {
+        total *= size;
+    }
+
+    std.debug.print("{}\n", .{total});
+}
+
+pub fn compareU64(context: void, a: u64, b: u64) bool {
+    _ = context;
+    return a > b;
+}
+
+pub fn sizeOfCircuit(start: *Point, val: *u64, seen: *std.ArrayList(*Point), alloc: std.mem.Allocator) !void {
+    for (start.connections.items) |connection| {
+        if (std.mem.indexOf(*Point, seen.items, &[_]*Point{connection}) != null) {
+            continue;
+        }
+
+        val.* += 1;
+        try seen.append(alloc, connection);
+        try sizeOfCircuit(connection, val, seen, alloc);
+    }
 }
 
 pub const Point = struct {
@@ -69,7 +107,23 @@ pub const Point = struct {
     y: f32,
     z: f32,
 
-    pub fn dist(self: *const Point, other: Point) f32 {
+    connections: std.ArrayList(*Point) = .{},
+
+    pub fn pointsTo(self: *Point, other: *Point) bool {
+        for (self.connections.items) |p| {
+            if (std.meta.eql(p, other)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    pub fn deinit(self: *Point, alloc: std.mem.Allocator) void {
+        self.connections.deinit(alloc);
+    }
+
+    pub fn dist(self: *const Point, other: *const Point) f32 {
         const x = std.math.pow(f32, self.x - other.x, 2);
         const y = std.math.pow(f32, self.y - other.y, 2);
         const z = std.math.pow(f32, self.z - other.z, 2);
@@ -79,10 +133,10 @@ pub const Point = struct {
 };
 
 pub const Pair = struct {
-    a: Point,
-    b: Point,
+    a: *Point,
+    b: *Point,
     dist: f32,
-    pub fn init(a: Point, b: Point) Pair {
+    pub fn init(a: *Point, b: *Point) Pair {
         return Pair{ .a = a, .b = b, .dist = a.dist(b) };
     }
 
@@ -91,76 +145,3 @@ pub const Pair = struct {
         return a.dist < b.dist;
     }
 };
-
-pub const Circuits = struct {
-    connections: std.ArrayList(Circuit) = .{},
-
-    pub fn connect(self: *Circuits, alloc: std.mem.Allocator, pair: Pair) !bool {
-        var added = false;
-
-        for (self.connections.items) |*circuit| {
-            if (circuit.alreadyHas(pair)) {
-                return false;
-            }
-
-            const try_append = try circuit.add(alloc, pair.a, pair.b) or try circuit.add(alloc, pair.b, pair.a);
-            if (try_append) {
-                added = true;
-                break;
-            }
-        }
-
-        if (!added) {
-            var circuit = Circuit{};
-            try circuit.points.append(alloc, pair.a);
-            try circuit.points.append(alloc, pair.b);
-
-            try self.connections.append(alloc, circuit);
-        }
-
-        return true;
-    }
-
-    pub fn deinit(self: *Circuits, alloc: std.mem.Allocator) void {
-        for (self.connections.items) |*conn| {
-            conn.deinit(alloc);
-        }
-        self.connections.deinit(alloc);
-    }
-};
-
-pub const Circuit = struct {
-    points: std.ArrayList(Point) = .{},
-
-    pub fn alreadyHas(self: *Circuit, pair: Pair) bool {
-        return contains(self.points.items, pair.a) and contains(self.points.items, pair.b);
-    }
-
-    pub fn add(self: *Circuit, alloc: std.mem.Allocator, a: Point, b: Point) !bool {
-        if (contains(self.points.items, a) and !contains(self.points.items, b)) {
-            try self.points.append(alloc, b);
-            return true;
-        }
-
-        return false;
-    }
-
-    pub fn deinit(self: *Circuit, alloc: std.mem.Allocator) void {
-        self.points.deinit(alloc);
-    }
-
-    pub fn compare(context: void, a: Circuit, b: Circuit) bool {
-        _ = context;
-        return a.points.items.len > b.points.items.len;
-    }
-};
-
-pub fn contains(vals: []const Point, find: Point) bool {
-    for (vals) |val| {
-        if (std.meta.eql(val, find)) {
-            return true;
-        }
-    }
-
-    return false;
-}
